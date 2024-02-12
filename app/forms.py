1a096ca4b3f3ed42b985a993d3764bb2a27c3f92
Rafin000 <marufulislam00000@gmail.com>
2024-02-12 11:08:13 +0600

    Error handing done

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,8 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 from app import db
 import sqlalchemy as sa
+from wtforms import TextAreaField
+from wtforms.validators import Length
 
 
 class LoginForm(FlaskForm):
@@ -33,3 +35,20 @@ class RegistrationForm(FlaskForm):
             User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email address.')
+        
+  
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
